1786921b7c2d07925a132229a4d6583c0aca108e
liubo <13621905107@163.com>
2021-03-04 11:10:34 +0800

    init

---
+++ RFriend/RFriend/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RFriend
+//
+//  Created by l on 2021/3/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
---
+++ RFriend/RFriend/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  RFriend
+//
+//  Created by l on 2021/3/4.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+@end
